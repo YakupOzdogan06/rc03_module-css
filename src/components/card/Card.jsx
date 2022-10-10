d8c2bf24690d61e4aebd1cdb7b45c60ef93a703e
Yakup <105318975+YakupOzdogan06@users.noreply.github.com>
2022-10-10 21:12:03 +0200

    fixed

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,6 +2,12 @@ import Button from "../button/Button";
 // import "./Card.css";
 import CardStyle from "./Card.module.css";
 
+//* module.css'de derleyici CSS class'larini alarak bunlari unique olacak
+//* sekilde yeniden adlandirir. (Ornek: card_title__XaSde)
+//* Bu durumda, global scope problemi ortadan kalktigi icin stillerin
+//* baska class tarafindan ezilmesi (overriding) engellenir.
+//* CSS module Webpack, Browsify gibi tool'lar ile kullanilabilir.
+
 const Card = ({ language, btn, img }) => {
   // console.log(props);
   // const { language, img, btn } = props;//!destr
